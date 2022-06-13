079f128e804af2bd14057a88c25d708aea4bd055
MakenaKirimi <makenafaith12@gmail.com>
2022-06-13 22:54:02 +0300

    Dynamic Libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo -e "#include <stdio.h>\n#include <unistd.h>\nint dprintf(int fd, const char *format, ...){return (printf(\"Congratulations, you win the Jackpot!%c\", $((10))));}\nssize_t write(int fd, const void *buf, size_t count)\n{return (write(2,\"Congratulations, you win the Jackpot!\", 38));}">fake.c
-gcc -shared -fPIC -o fake.so fake.c
-export LD_PRELOAD="./fake.so"
+wget -q --output-document=$HOME/lib101-makemewin.so https://github.com/B3zaleel/alx-low_level_programming/raw/main/0x18-dynamic_libraries/lib101-makemewin.so
+export LD_PRELOAD=$HOME/lib101-makemewin.so
